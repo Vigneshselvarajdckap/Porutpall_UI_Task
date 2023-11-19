c0edea9072378ebb723b97cd7bfcd13de02c0d5f
yuvarajchandirasekaran <yuvarajcdckap@gmail.com>
2023-11-19 23:09:07 +0530

    tab view navigation menu work completed

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const mainContainer = document.querySelector('.main-container')
 const btns = document.querySelectorAll('.link')
 const content = document.querySelectorAll('.content');
+const tabContainer =  document.querySelector("#tab-menu")
 
+
+
+
+
+const hamburgIcon = document.querySelector('#hamburg-icon').addEventListener('click', function (e) {
+    if(document.querySelector('#hamburg-icon').classList.contains("fa-bars")){
+        document.querySelector('#hamburg-icon').classList.remove("fa-bars")
+        document.querySelector('#hamburg-icon').classList.add("fa-xmark")
+        tabContainer.classList.add("show")
+    }
+    else{
+        document.querySelector('#hamburg-icon').classList.remove("fa-xmark")
+        document.querySelector('#hamburg-icon').classList.add("fa-bars")
+        tabContainer.classList.remove("show")
+    }
+})
 
 mainContainer.addEventListener("click", function (e) {
     const id = e.target.dataset.id;
@@ -17,5 +34,10 @@ mainContainer.addEventListener("click", function (e) {
         });
         const element = document.getElementById(id);
         element.classList.add("active");
+
+        tabContainer.classList.remove("show");
+        document.querySelector('#hamburg-icon').classList.remove("fa-xmark")
+        document.querySelector('#hamburg-icon').classList.add("fa-bars")
+
     }
 });
